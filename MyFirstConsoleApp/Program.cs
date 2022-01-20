800a536a85e85d174a11ff232c31921b0db24ec0
Ryan Stewart <Ryanps914@yahoo.com>
2022-01-19 19:27:16 -0500

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
